2b6ad1ec8fda273a1d5935b6af7c1e41592b1d92
solomondax <114422582+solomondax@users.noreply.github.com>
2023-01-22 18:05:31 +0530

    src pro

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState  } from 'react'
+
+function App() {
+   
+  const [data, setData] = useState({
+    username: " ",
+    password: " "
+  });
+  const { password, username } = data
+  
+  const  inputHandler = (e) => {
+   setData( e.target.name)
+  } 
+
+  const inputHand = (e) => {
+    setData(  e.target.value)
+    
+  }
+
+    const  submitHandler = (e) => {
+    e.preventDefault()
+    console.log(data)
+  }
+  
+  return (
+    <diV>
+      <center>
+      <form onSubmit={submitHandler}>
+        <input type="text" value={username} name="username" onChange={inputHandler} />
+        <br />
+        <input type="password" value={password} name ="password" onChange={inputHand}/><br />
+        <input type="submit" name ="sumit"/>
+      </form>
+    </center>
+    </diV>
+   )
+  }
+export default App 
+        
+        
+      
+    
+  
+  
+
+
+
+
